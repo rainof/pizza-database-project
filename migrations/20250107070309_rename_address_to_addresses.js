4e4c01ed755e11cfe2403d9dd2060e533523ac8c
rainof <13416303+rainof@users.noreply.github.com>
2025-01-07 14:28:51 +0700

    refactor(backend): rename table from "address" to "addresses"

---
+++ migrations/20250107070309_rename_address_to_addresses.js
@@ -0,0 +1,15 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.renameTable("address", "addresses");
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return;
+};
